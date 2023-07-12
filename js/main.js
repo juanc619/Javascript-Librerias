0e24f93ed32d09e22b2355ebcfc0154e853ee2e0
Juan Carlos <juancarlosvelasquez619@gmail.com>
2023-07-11 21:01:05 -0500

    luxon

--- js/main.js
+++ js/main.js
@@ -20,24 +20,29 @@
 
 //       })
 
-const toastify = document.querySelector("#toastify");
+// const toastify = document.querySelector("#toastify");
 
-toastify.addEventListener("click", () => {
+// toastify.addEventListener("click", () => {
 
-  Toastify({
-    text: "This is a toast",
-    duration: 3000,
-    destination: "https://github.com/apvarun/toastify-js",
-    newWindow: true,
-    close: true,
-    gravity: "top", // `top` or `bottom`
-    position: "left", // `left`, `center` or `right`
-    stopOnFocus: true, // Prevents dismissing of toast on hover
-    style: {
-      background: "linear-gradient(to right, #00b09b, #96c93d)",
-    },
-    onClick: function(){} // Callback after click
-  }).showToast();
+//   Toastify({
+//     text: "This is a toast",
+//     duration: 3000,
+//     destination: "https://github.com/apvarun/toastify-js",
+//     newWindow: true,
+//     close: true,
+//     gravity: "top", // `top` or `bottom`
+//     position: "left", // `left`, `center` or `right`
+//     stopOnFocus: true, // Prevents dismissing of toast on hover
+//     style: {
+//       background: "linear-gradient(to right, #00b09b, #96c93d)",
+//     },
+//     onClick: function(){} // Callback after click
+//   }).showToast();
   
-})
+// })
 
+const DateTime = luxon.DateTime;
+
+const now = DateTime.now();
+
+console.log(now.toLocaleString(DateTime.DATETIME_HUGE));
